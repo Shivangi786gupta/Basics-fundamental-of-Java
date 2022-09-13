b5f2a258dbc17fa114dc6645c0e0379d3161d4d4
Shivangi Gupta <102048213+Shivangi786gupta@users.noreply.github.com>
2022-09-13 13:57:50 +0530

    Printing right triangle using serial numbers

---
+++ Number1.java
@@ -0,0 +1,25 @@
+/**
+Write a program for the following:
+
+0
+1 2
+3 4 5
+6 7 8 9
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+class Number1
+{
+public static void main(String[] args)
+{
+int k=0,num=0;
+for(int i=1;i<=4;i++){
+num++;
+for(int j=1;j<=num;j++){
+{
+System.out.print(k++ +" ");
+}
+}
+System.out.println();
+}
+}
+}
