036ddbbde54d7b182fa5274d9cf12d22de280142
Shivangi Gupta <102048213+Shivangi786gupta@users.noreply.github.com>
2022-09-13 13:46:05 +0530

    Printing right triangle

---
+++ NestedLoop5.java
@@ -0,0 +1,23 @@
+/**
+Write a program for the following:
+
+*
+**
+***
+****
+*****
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/class NestedLoop5
+{
+public static void main(String[] args)
+{
+int star=0;
+for (int i=1;i<=5;i++){
+star++;
+	for(int j=1;j<=star;j++){
+	System.out.print("*");
+	}
+
+	System.out.println();
+}
+}
+}
