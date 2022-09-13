d7833c9e98faf96a15eba52a4987bef22107d8b3
Shivangi Gupta <102048213+Shivangi786gupta@users.noreply.github.com>
2022-09-13 13:53:45 +0530

    Printing opposite of left triangle with some diff. sign

---
+++ NestedLoop8.java
@@ -0,0 +1,27 @@
+/**
+Write a program for the following:
+  
+ *****
+  ****
+   @**
+    **
+     *  
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+class NestedLoop8
+{
+public static void main(String[] args)
+{
+int star=6,space=-1;
+	for(int i=1;i<=5;i++){
+space++;
+star--;
+	for(int j=1;j<=space;j++){
+	System.out.print(" ");
+	}
+	for(int j=1;j<=star;j++){
+	System.out.print("*");
+	}
+	System.out.println();
+}
+}
+}
