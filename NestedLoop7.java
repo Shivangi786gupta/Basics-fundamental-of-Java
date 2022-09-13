ead4fe55c4e9e21dcfa8c7eb64ca5b1a2c597ddf
Shivangi Gupta <102048213+Shivangi786gupta@users.noreply.github.com>
2022-09-13 13:51:39 +0530

    Printing left triangle

---
+++ NestedLoop7.java
@@ -0,0 +1,27 @@
+/**
+Write a program for the following:
+  
+    *
+   **
+  ***
+ ****
+*****
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+class NestedLoop7
+{
+public static void main(String[] args)
+{
+int star=0,space=5;
+	for(int i=1;i<=5;i++){
+star++;
+space--;
+	for(int j=1;j<=space;j++){
+	System.out.print(" ");
+	}
+	for(int j=1;j<=star;j++){
+	System.out.print("*");
+	}
+	System.out.println();
+}
+}
+}
